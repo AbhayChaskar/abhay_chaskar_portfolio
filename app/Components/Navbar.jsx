12f47d70011d711284771f6da6401808b0b1ed36
AbhayChaskar <abhayrc.521@gmail.com>
2025-02-16 02:25:02 +0530

    Added sidemenu for smaller device

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import { FiDownload } from "react-icons/fi";
 import { IoMoonSharp } from "react-icons/io5";
 import { CgMenuRightAlt } from "react-icons/cg";
+import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
+    const sideMenuRef = useRef()
+    
+    const openMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(-16rem)';
+    }
+    const closeMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(16rem)';
+    }
+
   return (
     <>
     <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
@@ -29,7 +39,23 @@ const Navbar = () => {
                 <IoMoonSharp size={20}/>
             </button>
             <a href='' className='hidden lg:flex items-center gap-3 px-6 py-2.5 border border-gray-400 rounded-full ml-4'>Resume <FiDownload size={21}/></a>
+            <button className='block md:hidden ml-3'>
+                <CgMenuRightAlt size={27} onClick={openMenu}/>
+            </button>
         </div>
+
+        {/* Side menu for small devices */}
+        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 px-10 py-20 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-gray-50 transition duration-500'>
+            <div className='absolute right-5 top-4'>
+                <IoClose size={30} className='cursor-pointer' onClick={closeMenu}/>
+            </div>
+            <li><a href='' onClick={closeMenu}>Home</a></li>
+            <li><a href='' onClick={closeMenu}>About me</a></li>
+            <li><a href='' onClick={closeMenu}>Skills</a></li>
+            <li><a href='' onClick={closeMenu}>Experience</a></li>
+            <li><a href='' onClick={closeMenu}>Projects</a></li>
+            <li><a href='#contact' onClick={closeMenu}>Contact</a></li>
+        </ul>
     </nav>
     </>
   )
