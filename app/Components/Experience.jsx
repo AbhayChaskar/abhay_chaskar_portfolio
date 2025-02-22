f89aaddd145be4d24bb4056a0e4312447e0fa767
AbhayChaskar <abhayrc.521@gmail.com>
2025-02-22 16:25:24 +0530

    added custom scrollbar

--- app/Components/Experience.jsx
+++ app/Components/Experience.jsx
@@ -9,7 +9,7 @@ const Experience = () => {
 
          <div className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
           <div className='flex-1'>
-              <p className='mb-10 mx-auto'>
+              <p className='text-center mx-auto mb-10'>
                With 4 years of experience in development and expertise in both Front-end & Back-end technologies, I have been delivering user experience & client-focused projects. Enhanced load times by 30% and had an 85% client retention rate through effective communication and co-ordination with cross-functional teams.
               </p>
 
--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth custom-scrollbar">
       <body
         className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden`}
       >
