0217dd0aba3605a1e8a9b5780ffe418e5ed7d584
AbhayChaskar <abhayrc.521@gmail.com>
2025-03-01 13:25:22 +0530

    removed console error

--- app/Components/Experience.jsx
+++ app/Components/Experience.jsx
@@ -15,22 +15,20 @@ const Experience = () => {
 
           <ul className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6'>
             {experienceData.map((experience, index) => (
-              <>
-                <li key={index} className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-gray'>
-                    <div className='flex items-center mt-2 gap-20'>
-                        <Image src={experience.icon} alt={experience.title} className='flex w-16'/>
-                        <p className='text-black/60 mx-auto'>{experience.duration}</p>
-                    </div>
-                    <div>
-                      <h3 className='my-2 font-semibold text-gray-600'>{experience.title}</h3>
-                      <ul className='list-disc mx-4'>
-                        {experience.description.map((description, descriptionIndex) => (
-                          <li key={descriptionIndex} className='text-gray-500 text-sm'>{description}</li>
-                        ))}
-                      </ul>
-                    </div>
-                </li>
-              </>
+              <li key={index} className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-gray'>
+                  <div className='flex items-center mt-2 gap-20'>
+                      <Image src={experience.icon} alt={experience.title} className='flex w-16'/>
+                      <p className='text-black/60 mx-auto'>{experience.duration}</p>
+                  </div>
+                  <div>
+                    <h3 className='my-2 font-semibold text-gray-600'>{experience.title}</h3>
+                    <ul className='list-disc mx-4'>
+                      {experience.description.map((description, descriptionIndex) => (
+                        <li key={descriptionIndex} className='text-gray-500 text-sm'>{description}</li>
+                      ))}
+                    </ul>
+                  </div>
+              </li>
             ))}
           </ul>
         </div>
