3620d578b3e7adbe506a192a28850c6fe6267da9
AbhayChaskar <abhayrc.521@gmail.com>
2025-04-04 23:51:07 +0530

    motion for header text

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -19,9 +19,9 @@ const Header = () => {
         <Image src={assets.profile_image} alt='profile_image' className='rounded-full w-32'/>
       </motion.div>
 
-      <h3 className='flex item-end gap-1.5 font-Ovo text-xl md:text-2xl mb-3 '>Hey! I'm Abhay Chaskar <PiHandPeace size={27}/></h3>
-      <h3 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>Frontend/UI Developer</h3>
-      <p className='max-w-2xl mx-auto'>I have over 4 years of full-time experience in the IT industry, where I have developed and managed SPAs using ReactJS and related libraries or frameworks.</p>
+      <motion.h3 initial={{y: -10, opacity:0}} whileInView={{y:0, opacity:1}} transition={{duration: 0.5, delay: 0.2}} className='flex item-end gap-1.5 font-Ovo text-xl md:text-2xl mb-3 '>Hey! I'm Abhay Chaskar <PiHandPeace size={27}/></motion.h3>
+      <motion.h3 initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration: 0.7, delay: 0.4}} className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>Frontend/UI Developer</motion.h3>
+      <motion.p initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration: 0.5, delay: 0.6}} className='max-w-2xl mx-auto'>I have over 4 years of full-time experience in the IT industry, where I have developed and managed SPAs using ReactJS and related libraries or frameworks.</motion.p>
       <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
         <a href='' className='py-2.5 px-6 w-max flex items-center gap-2 justify-between bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 dark:border-none dark:bg-white/80 dark:hover:bg-white dark:duration-500 dark:text-black' onClick={handleConnectButton}>Let's Connect <IoIosArrowDropright size={20}/></a>
       </div>
